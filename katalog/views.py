39e4a09be06175413f8c7b667588c3c20dec625e
jonathanadriel <jonathanadriels@gmail.com>
2022-09-14 05:50:49 +0700

    Done point 1, 2, and 3

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+
+def show_catalog(request):
+    context = {
+        'name': 'Jonathan Adriel',
+        'student_id': "2106750692",
+        'catalog_list': CatalogItem.objects.all(),
+    }
+    return render(request, "katalog.html", context)
